149860be8d72f915c04a10340b0ad8fa06c173b8
Shanmuga Prabu <shanmuga.prabu@compassitesinc.com>
2016-06-08 16:27:53 +0530

    Front End - Bootstrap integration - Login page - Blade templates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,14 @@ class HomeController extends Controller
     public function Index()
     {
         $forex = new Forex;
+        $forexData['title']='List';
         $forexData['response']=$forex->all(array('currency','currencyrate'));
         return view('forex',$forexData);
+    }
+    
+    public function Login()
+    {
+        $data['title']='Login';
+        return view('login',$data);
     }
 }
--- resources/views/forex.blade.php
+++ resources/views/forex.blade.php
@@ -1,19 +1,20 @@
-<!doctype HTML>
-<html>
-    <head>
-        <title>Forex Rates</title>
-    </head>
-    <body>
-        <table>
-            <tr>
-                <td>Currency Code</td><td>Currency Rate</td>
-            </tr>
-            @foreach($response as $forexData)
-            <tr>
-                <td>{{ $forexData->currency }}</td>
-                <td>{{ $forexData->currencyrate }}</td>
-            </tr>
-            @endforeach
-        </table>
-    </body>
-</html>
+@extends('layout')
+@section('title')
+    {{ $title }}
+@endsection
+
+@section('content')
+<h2>Forex Rates</h2>
+<hr/>
+<table class="table table-striped table-hover">
+    <tr>
+        <th>Currency Code</th><th>Currency Rate</th>
+    </tr>
+    @foreach($response as $forexData)
+    <tr>
+        <td>{{ $forexData->currency }}</td>
+        <td>{{ $forexData->currencyrate }}</td>
+    </tr>
+    @endforeach
+</table>
+@endsection
